657881f3b1c7f89f959be9bf87ef06683cdef371
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2021-05-16 23:50:39 -1000

    ep08 project code

---
+++ pong-08/Paddle/AiPaddle.gd
@@ -0,0 +1,25 @@
+extends Paddle08
+
+class_name AiPaddle08
+
+const _CHASE_BUFFER: float = 15.0
+
+func _init(box:BoundBox08) -> void:
+	_boundBox = box
+	_pos = Vector2(_boundBox.getSize().x - (_padding + _size.x), _boundBox.getHalfHeight() - _halfHeight) # specific to subclass
+	_resetPos = _pos
+	_rect = Rect2(_pos, _size)
+
+func checkMovement(delta: float, ballPos: Vector2):
+	if ballPos.y <= (_pos.y + _halfHeight) - _CHASE_BUFFER:
+		moveUp(delta)
+		updatePosition()
+	elif ballPos.y >= (_pos.y + _halfHeight) + _CHASE_BUFFER:
+		moveDown(delta)
+		updatePosition()
+
+func moveUp(delta: float) -> void:
+	_pos.y -= _speed.y * delta
+
+func moveDown(delta: float) -> void:
+	_pos.y += _speed.y * delta
